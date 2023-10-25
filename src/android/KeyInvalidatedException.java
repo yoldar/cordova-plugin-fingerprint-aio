cb96b116f0ebb2e19260420cc21701688f828100
= <=>
2023-10-25 23:01:02 +0600

    refactor: Removed Android support

--- src/android/KeyInvalidatedException.java
+++
@@ -1,7 +0,0 @@
-package de.niklasmerz.cordova.biometric;
-
-class KeyInvalidatedException extends CryptoException {
-    KeyInvalidatedException() {
-        super(PluginError.BIOMETRIC_NO_SECRET_FOUND);
-    }
-}
